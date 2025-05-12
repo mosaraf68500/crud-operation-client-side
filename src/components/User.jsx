19ac546f0de505bbc688bedd774ee3673cc5774b
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-13 00:06:18 +0600

    insert user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
+const handleAddUser =e=>{
+    e.preventDefault();
+    const name=e.target.name.value;
+    const email=e.target.email.value;
+    const newUser={name,email};
+    console.log(newUser);
+
+    fetch('http://localhost:3000/users',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(newUser)
+    })
+    .then(res=>res.json())
+    .then(data =>{
+        console.log('after the insert data',data);
+    })
+}
 const User = () => {
     return (
         <div className='w-11/12 mx-auto bg-gray-200 rounded-3xl'>
             <div>
-                <form>
+                <form onSubmit={handleAddUser}>
                     <div>
                         <input className='bg-white border-2 w-[300px] p-3 rounded-3xl my-6' placeholder=' Enter Your Name' type="text" name="name" id="" />
                     </div>
